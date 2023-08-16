cdac52ea09adc0a4abf60c080aa5661984fdaf9d
Gurdaman Singh <>
2023-08-16 22:51:52 +0530

    second with comments

--- src/main/java/firefoxFramework/Assigment1.java
+++ src/main/java/firefoxFramework/Assigment1.java
@@ -12,28 +12,34 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Assigment1 {
 	public static void main(String[] args) throws InterruptedException {
-
+		//Initializing FireFoxDriver
 		WebDriver driver = new FirefoxDriver();
+		
+	//adding waits 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		//navigating to the trello website
 		driver.get("https://trello.com/");
 
 		driver.findElement(By.linkText("Log in")).click();
 
+		//adding the user details
 		driver.findElement(By.id("user")).sendKeys("gurdamansingh2112@gmail.com");
 
 		driver.findElement(By.id("login")).click();
 
 		WebElement login = driver.findElement(By.id("login-submit"));
 
-		
 			Thread.sleep(1000);
 		
 		driver.findElement(By.name("password")).sendKeys("Gur2112@18");
 		login.click();
 		
+		
+		//after sucessfull login create a board 
 		driver.findElement(By.xpath("//*[text()='Create new board']")).click();
 		
 		 String generatedString = randomString(10);
@@ -42,17 +48,8 @@ public class Assigment1 {
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//button[@data-testid='create-board-submit-button']")).click();
 		
-//		String boardXpath = "//div[@title='"+ generatedString +"']";
-//
-//		WebElement board = driver.findElement(By.xpath(boardXpath));
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(board));
-//		board.click();
-//
-
-	//	driver.findElement(By.xpath("//*[text()='Add another list']")).click();
 		
-		
+		//after board is created now create lists as mentioned
 		driver.findElement(By.xpath("//*[@placeholder='Enter list title…']")).sendKeys("List A");
 
 		driver.findElement(By.xpath("//*[@value='Add list']")).click();
@@ -61,6 +58,8 @@ public class Assigment1 {
 
 		driver.findElement(By.xpath("//*[@value='Add list']")).click();
 
+		
+		//add a card in list A
 		WebElement addcard = driver
 				.findElement(By.xpath("//h2[text()='List A']/ ../following-sibling::div//*[text()='Add a card']"));
 
@@ -72,12 +71,14 @@ public class Assigment1 {
 		
 		Thread.sleep(1000);
 		
+		//this is for drag and drop
 	  Actions action = new Actions(driver); 
 	  WebElement card1 =	driver.findElement(By.xpath("//*[text()='card1']"));
 	  WebElement listB =	driver.findElement(By.xpath("//h2[text()='List B']/ ../ .."));
 	  action.dragAndDrop(card1, listB).build().perform();
 	  card1 =	driver.findElement(By.xpath("//*[text()='card1']"));
 	  
+	  //getting the co-ordinates of the card
 	  Point location = card1.getLocation();
       System.out.println("X, Y - coordinates : " + location);	
       
@@ -85,7 +86,7 @@ public class Assigment1 {
 		
 
 	}
-	
+	//this menthod is to generate random Sting for a given length
 	public static String randomString(int l) {
 		 String Str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz";
 
